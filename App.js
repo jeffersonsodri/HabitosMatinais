acf359a93fbc1c2dc9a0ea04feffa9240460493c
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2019-07-29 19:45:43 -0400

    Commit inicial do projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Jefferson GOSTOSO DEMAIS!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
