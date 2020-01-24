51e8da9ff60f45fdbc56dc8d96075473b86c3f3f
Jefferson Souza Rodrigues <jsodricacula@hotmail.com>
2020-01-24 02:17:09 -0400

    Adicionando controllers e models to application

---
+++ BackEnd/src/models/Activity.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require('mongoose');
+
+
+ const ActivictySchema =  new Schema({
+
+    
+
+ });
+
+
+
+ module.exports = model('Activity', ActivictySchema);
---
+++ BackEnd/src/models/User.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require('mongoose');
+
+
+const UserSchema = new Schema({
+
+
+
+});
+
+
+module.exports = model('User', UserSchema);
